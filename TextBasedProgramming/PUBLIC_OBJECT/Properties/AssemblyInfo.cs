a0ce55b388109b5a58d74e5c1918e7e774f6957b
tranquocbao2903 <tranquocbao2903@gmail.com>
2017-03-18 12:12:47 +0700

    [Added] PUBLIC class library

--- TextBasedProgramming/PUBLIC_OBJECT/Properties/AssemblyInfo.cs
+++ TextBasedProgramming/PUBLIC_OBJECT/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PUBLIC")]
+[assembly: AssemblyTitle("PUBLIC_OBJECT")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PUBLIC")]
+[assembly: AssemblyProduct("PUBLIC_OBJECT")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2b640b13-4f04-42a8-bd74-d71107f2f0be")]
+[assembly: Guid("c8ffa33c-f995-4d88-9f9b-9406bc7ee8e3")]
 
 // Version information for an assembly consists of the following four values:
 //
